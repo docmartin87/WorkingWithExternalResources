5c76f04d0ae19469fa7fdb6532ddd4f85e4a49f0
docmartin87 <doc.martin87@gmail.com>
2021-01-06 12:04:25 +0000

    initial

--- 02-ConsumingAPIsUsingJavaScript/01-xhr/main.js
+++ 02-ConsumingAPIsUsingJavaScript/01-xhr/main.js
@@ -1,11 +1,10 @@
-var xhr = new XMLHttpRequest();
+var xhr = new XMLHttpRequest();                                             // Creates a new instance of XMLHttpRequest object
 
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
+xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");                     // Open method with first argument of GET (used for retrieving) followed by the URL
+xhr.send();                                                                 // Sends the request
+
+xhr.onreadystatechange = function() {                                       // 
+    if (this.readyState == 4 && this.status == 200) {                       // xhr ready state of 4 means operation is complete && http status code 200 means OK
+        document.getElementById("data").innerHTML = this.responseText;      // gets the element by id of data from the dom and change the inner html to the response text
     }
 };
-
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-
-xhr.send();
